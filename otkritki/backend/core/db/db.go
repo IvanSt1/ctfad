29dd56cb0b600a98003023782f6428f5151bc8c7
Ivan <ivanstupnikov4@gmail.com>
2025-04-26 11:44:10 +0300

    a

--- otkritki/backend/core/db/db.go
+++ otkritki/backend/core/db/db.go
@@ -3,15 +3,17 @@ package db
 import (
     "fmt"
     "os"
+    "time"
 
     "github.com/IvanSt1/ctfad/otkritki/backend/core/models"
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
+    "gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
 
-// init устанавливает соединение с БД и выполняет авто-миграцию
+// init устанавливает соединение с БД, ожидая готовности сервиса
 func init() {
     dsn := fmt.Sprintf(
         "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -23,14 +25,24 @@ func init() {
     )
 
     var err error
-    db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    // Пытаемся подключиться с ретраями
+    for i := 0; i < 10; i++ {
+        db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+            Logger: logger.Default.LogMode(logger.Silent),
+        })
+        if err == nil {
+            break
+        }
+        fmt.Printf("[db] connection attempt %d failed: %v\n", i+1, err)
+        time.Sleep(5 * time.Second)
+    }
     if err != nil {
-        panic("failed to connect to database: " + err.Error())
+        panic(fmt.Sprintf("failed to connect to database after retries: %v", err))
     }
 
-    // Миграция моделей GiftCard и User
+    // Миграция моделей
     if err := db.AutoMigrate(&models.GiftCard{}, &models.User{}); err != nil {
-        panic("failed to auto-migrate models: " + err.Error())
+        panic(fmt.Sprintf("failed to auto-migrate models: %v", err))
     }
 }
 
